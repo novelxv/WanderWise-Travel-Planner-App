671af8b87fcbd2f4e70906e8c55cfe11af7df212
novelxv <novelyaputriramadhani11@gmail.com>
2024-05-11 21:10:17 +0700

    feat: add setup

---
+++ src/app/utils/mymath/addition.py
@@ -0,0 +1,4 @@
+class Math:
+    @staticmethod
+    def add(x: float, y: float) -> float:
+        return x + y
---
+++ src/main.py
@@ -0,0 +1,10 @@
+import src.app.utils.mymath.addition as addition
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
+    print(addition.Math.add(1, 2))
