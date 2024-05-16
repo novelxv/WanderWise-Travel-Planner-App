731abd99dc6cba131fb26965e8cede49169d48f0
keflay02 <zulclashkusynk2@gmail.com>
2024-05-16 17:02:25 +0700

    feat:add homepage and button

---
+++ src/ui/pages/homepage.py
@@ -0,0 +1,55 @@
+import sys
+from PyQt5 import QtWidgets, QtGui, QtCore
+from PyQt5.QtWidgets import QLabel, QVBoxLayout, QSpacerItem, QSizePolicy, QWidget
+from PyQt5.QtGui import QPixmap, QPalette, QBrush
+from PyQt5.QtCore import Qt
+
+# Import kelas tombol yang baru Anda buat
+from src.ui.components.hellobutton.hellobutton import OvalButton
+
+class HomePage(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('Home Page')
+        self.setGeometry(300, 100, 1500, 600)  # Adjust window position and size
+
+        # Set background image
+        self.pixmap = QPixmap('img/bg/Homepage2.png')
+        self.updateBackground()
+
+        layout = QVBoxLayout()
+
+        # Create spacer to push content to the center
+        spacer_top = QSpacerItem(20, 200, QSizePolicy.Minimum, QSizePolicy.Expanding)
+        layout.addItem(spacer_top)
+
+        # Tambahkan tombol yang baru Anda buat
+        custom_button1 = OvalButton("See All Destination Here!", "#263B4E", 11)
+        layout.addWidget(custom_button1, alignment=Qt.AlignCenter)
+
+        custom_button2 = OvalButton("See Articles Here!", "#263B4E", 11)
+        layout.addWidget(custom_button2, alignment=Qt.AlignCenter)
+
+        # Create spacer to push content to the center
+        spacer_bottom = QSpacerItem(20, 200, QSizePolicy.Minimum, QSizePolicy.Expanding)
+        layout.addItem(spacer_bottom)
+
+        self.setLayout(layout)
+
+    def resizeEvent(self, event):
+        super().resizeEvent(event)
+        self.updateBackground()
+
+    def updateBackground(self):
+        palette = QPalette()
+        palette.setBrush(QPalette.Background, QBrush(self.pixmap.scaled(self.size(), Qt.IgnoreAspectRatio, Qt.SmoothTransformation)))
+        self.setPalette(palette)
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = HomePage()
+    window.show()
+    sys.exit(app.exec_())
