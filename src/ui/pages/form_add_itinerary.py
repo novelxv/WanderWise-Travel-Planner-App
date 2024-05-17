3a2d9c30bbf6ce5cddb604d1b31991a13f39793d
saadabha <saadabdulhakim08@gmail.com>
2024-05-17 19:44:29 +0700

    feat: add edit and add itinerary

--- src/ui/pages/form_add_itinerary.py
+++ src/ui/pages/form_add_itinerary.py
@@ -1,8 +1,9 @@
 import sys
 import os
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QScrollArea, QFrame, QLabel, QHBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QScrollArea, QFrame, QLabel
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtCore import Qt
+from src.ui.components.ovalbutton.ovalbutton import *
+from src.ui.components.Form.form_box import FormBox
 
 class FormAddItinerary(QWidget):
     def __init__(self, parent=None):
@@ -25,8 +26,6 @@ class FormAddItinerary(QWidget):
             print(f"Image not found at {path}")
         else:
             print(f"Image found at {path}")
-
-        # location_container = 
 
         # Create background label
         bgLabel = QLabel(self)
@@ -52,13 +51,41 @@ class FormAddItinerary(QWidget):
         content_widget.setMinimumWidth(scroll_area.width())
         scroll_area.setWidget(content_widget)
 
-        # LAYOUT
-        layout = QVBoxLayout(content_widget)
-        # Example content to fill the scroll area
-        for i in range(10):
-            label = QLabel(f".", content_widget)
-            label.setStyleSheet("background: none;")
-            layout.addWidget(label)
+        # Add the FormBox instance For Location
+        form_box_location = FormBox("Amusement Park", self, 950)
+        form_box_location.setParent(content_widget)
+        form_box_location.move(100, 180)
+
+        # add the FormBox For Start Time
+        form_box_stime = FormBox("07.00", self, 400)
+        form_box_stime.setParent(content_widget)
+        form_box_stime.move(100, 290)
+
+        # add the FormBox For End Time
+        form_box_etime = FormBox("11.30", self, 400)
+        form_box_etime.setParent(content_widget)
+        form_box_etime.move(545, 290)
+
+       # Add the FormBox instance For Cost/Ticket
+        form_box_cost = FormBox("XX.XXX.XXX", self, 950)
+        form_box_cost.setParent(content_widget)
+        form_box_cost.move(100, 400)
+
+        # Add the FormBox instance For description
+        form_box_description = FormBox("Lorem ipsum dolor sit amet, consectetur...", self, 950)
+        form_box_description.setParent(content_widget)
+        form_box_description.move(100, 510)
+
+        # Add the FormBox instance For notes
+        form_box_notes = FormBox("Lorem ipsum dolor sit amet...", self, 950)
+        form_box_notes.setParent(content_widget)
+        form_box_notes.move(100, 625)
+
+        # Add the oval button done for submitting the form
+        oval_button = OvalButton("Done", "#69C99E", 35, self)
+        oval_button.setParent(content_widget)
+        oval_button.setFixedSize(149, 70)
+        oval_button.move(450, 700)
 
         # Adding the scroll area to the main layout
         main_layout = QVBoxLayout(container)
