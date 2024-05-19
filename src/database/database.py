023bc46de4edb826f4d3425825e54e5925a065cf
novelxv <novelyaputriramadhani11@gmail.com>
2024-05-19 19:14:31 +0700

    feat: kayla fix database

--- src/database/database.py
+++ src/database/database.py
@@ -6,19 +6,26 @@ def connect_db():
     path = os.path.join(cwd, 'wanderwise.db')
 
     if not os.path.exists(path):
-        print("Creating database!")
-        conn = sqlite3.connect(path)
-        cursor = conn.cursor()
-        with open(os.path.join(cwd, 'schema.sql')) as f:
-            cursor.executescript(f.read()) 
-        conn.commit()
-        conn.close()
-        return conn,cursor
+        return create_db(path,cwd)
     else:
         conn = sqlite3.connect(path)
         cursor = conn.cursor()
+        # check if article dummy exists
+        articles = cursor.execute('SELECT * FROM t_artikel').fetchall()
+        if len(articles) == 0:
+            return create_db(path, cwd)
         return conn,cursor
     
+def create_db(path,cwd):
+    print("Creating database!")
+    conn = sqlite3.connect(path)
+    cursor = conn.cursor()
+    with open(os.path.join(cwd, 'schema.sql')) as f:
+        cursor.executescript(f.read()) 
+    conn.commit()
+    # conn.close()
+    return conn,cursor
+
 def clear_db():
     tables = ["t_itinerary", "t_artikel", "t_destinasi"]
     for table in tables:
--- tests/test_artikel_controller.py
+++ tests/test_artikel_controller.py
@@ -3,7 +3,7 @@ from unittest import TestCase
 
 from src.controller.artikel_controller import ArtikelController
 from src.models.artikel import Article
-from src.database.database import clear_db
+from src.database.database import *
 
 class TestArtikelController(TestCase):
     def setUp(self):
@@ -20,19 +20,20 @@ class TestArtikelController(TestCase):
             self.controller.add_artikel(entry["judul"], entry["konten"], entry["penulis"], entry["tanggal_rilis"])
             list_artikel.append(Article(i+1, entry["judul"], entry["konten"], entry["penulis"], entry["tanggal_rilis"]))
         result = self.controller.get_all_artikel()
-        assert result[0].judul == "Things to bring in your vacation"
-        assert result[0].konten == "Hello"
-        assert result[0].penulis == "J.K.Rowling"
-        assert result[0].tanggal_rilis == date(2023, 6, 20)
+        second_to_last_artikel = len(result) - 2
+        assert result[second_to_last_artikel].judul == "Things to bring in your vacation"
+        assert result[second_to_last_artikel].konten == "Hello"
+        assert result[second_to_last_artikel].penulis == "J.K.Rowling"
+        assert result[second_to_last_artikel].tanggal_rilis == date(2023, 6, 20)
 
     def test_get_artikel_by_id(self):
         list_artikel = []
         for i,entry in enumerate(self.tests):
             self.controller.add_artikel(entry["judul"], entry["konten"], entry["penulis"], entry["tanggal_rilis"])
             list_artikel.append(Article(i+1, entry["judul"], entry["konten"], entry["penulis"], entry["tanggal_rilis"]))
-        result = self.controller.get_artikel_by_id(1)
-
-        self.assertEqual(result.judul, list_artikel[0].judul)
-        self.assertEqual(result.konten, list_artikel[0].konten)
-        self.assertEqual(result.penulis, list_artikel[0].penulis)
-        self.assertEqual(result.tanggal_rilis, list_artikel[0].tanggal_rilis)
+        result = self.controller.get_artikel_by_id(6)
+        
+        self.assertEqual(result.judul, list_artikel[1].judul)
+        self.assertEqual(result.konten, list_artikel[1].konten)
+        self.assertEqual(result.penulis, list_artikel[1].penulis)
+        self.assertEqual(result.tanggal_rilis, list_artikel[1].tanggal_rilis)
