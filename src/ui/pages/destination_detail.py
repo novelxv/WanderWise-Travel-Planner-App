70e6a561c50b75a9f134a4761bd607839f8011cf
kaylanamira <kaylanmr04@gmail.com>
2024-05-19 19:47:28 +0700

    fix : edit and delete button on destination detail

--- src/ui/pages/destination_detail.py
+++ src/ui/pages/destination_detail.py
@@ -7,6 +7,7 @@ from src.ui.components.ovalbutton.ovalbutton import OvalButtonIcon
 from src.ui.components.dropdown.dropdown import DropDown
 from src.ui.pages.form_edit_destination import FormEditDestination
 from src.ui.components.popup.pop_up_confirm import ConfirmationDialog
+from src.ui.components.destinationscard.destinationscard import CustomButton
 from src.ui.components.progressbar.progressbar import ProgressBarWindow
 from src.controller.destinasi_controller import *
 from src.ui.components.backbutton.backbutton import BackButton
@@ -149,15 +150,19 @@ class DestinationDetail(QWidget):
 
     def handle_edit_button_click(self):
         print("Edit button clicked!")
-        # Add your logic for edit button click here
+        self.edit_destination_form = FormEditDestination(self)
+        self.edit_destination_form.done_signal.connect(self.on_done_signal)  # Connect the signal
+        self.edit_destination_form.setWindowModality(Qt.ApplicationModal)
+        self.edit_destination_form.setGeometry(40, 80, 800, 600)  # Set fixed size and position
+        self.edit_destination_form.show()
 
     def handle_delete_button_click(self):
         print("Delete button clicked!")
-        # Add your logic for delete button click here
+        self.destinasi_controller.delete_destinasi(destination_id)
+        self.main_window.stacked_widget.setCurrentIndex(3)
 
     def handle_budgeting_button_click(self):
         print("Budgeting button clicked!")
-        print("CEK KEGANTI GA TABUNGAN", self.destinasi.tabungan) # debug
         budgeting_page = BudgetingWindow(self.destinasi, self.main_window, self)
         self.main_window.stacked_widget.addWidget(budgeting_page)
         self.main_window.stacked_widget.setCurrentWidget(budgeting_page)
@@ -170,7 +175,40 @@ class DestinationDetail(QWidget):
 
     def handle_back_button_click(self):
         print("Back button clicked!")
-        # Add your logic for back button click here
+
+    def on_done_signal(self, edited_destination):
+        for i in range(6): # debug
+            print(edited_destination[i]) # debug
+        self.destinasi_controller.edit_destinasi(destination_id, edited_destination[0], edited_destination[1], edited_destination[2], edited_destination[3], edited_destination[4], edited_destination[5])
+        self.refresh_destinations()
+        self.show_all_destinations()
+
+    def refresh_destinations(self, destinations=None):
+        if destinations is None:
+            destinations = self.destinations
+
+        # Clear the existing cards
+        for i in reversed(range(self.grid_layout.count())): 
+            self.grid_layout.itemAt(i).widget().setParent(None)
+
+        # Get the latest destinations data
+        self.destinations = self.destinasi_controller.get_all_destinasi()
+
+        # Populate the grid layout with destination cards
+        for index, destination in enumerate(destinations):
+            print("ID", destination.destinasi_id)
+            icon_path = "img/icons/destination.jpg"
+            text = destination.nama  
+            width = 400  # Set the width for each card
+            card = CustomButton(icon_path, text, width, destination.destinasi_id)
+            card.clicked_with_id.connect(self.open_destination_detail)
+            row = index // 3
+            col = index % 3
+            self.grid_layout.addWidget(card, row, col)
+            card.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+
+    def show_all_destinations(self):
+        self.refresh_destinations()
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
